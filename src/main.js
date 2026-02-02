b1590ea234a0c5ac6d38702221eb00aae5b2a163
🌐 UtrechtOG 🌐 <Calimali2006@gmail.com>
2026-02-02 22:05:45 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,1 +1,19 @@
+import { ctx } from './canvas.js';
+import { CONFIG } from './config.js';
 
+function update() {
+  // später Logik
+}
+
+function render() {
+  ctx.fillStyle = 'black';
+  ctx.fillRect(0, 0, CONFIG.WIDTH, CONFIG.HEIGHT);
+}
+
+function gameLoop() {
+  update();
+  render();
+  requestAnimationFrame(gameLoop);
+}
+
+gameLoop();
