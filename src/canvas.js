113a9f7871089f7212f0f454adb206f1b859ed56
🌐 UtrechtOG 🌐 <Calimali2006@gmail.com>
2026-02-02 22:05:04 +0100

    Update canvas.js

--- src/canvas.js
+++ src/canvas.js
@@ -1,1 +1,9 @@
+import { CONFIG } from './config.js';
 
+const canvas = document.getElementById('game');
+const ctx = canvas.getContext('2d');
+
+canvas.width = CONFIG.WIDTH;
+canvas.height = CONFIG.HEIGHT;
+
+export { canvas, ctx };
