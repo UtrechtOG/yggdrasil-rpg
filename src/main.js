1b9873c7a4898a5ac24ffc7e2b57161e9e59b4ae
🌐 UtrechtOG 🌐 <Calimali2006@gmail.com>
2026-02-03 13:52:59 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,48 +4,28 @@ import './touch.js';
 
 import { ctx } from './canvas.js';
 import { CONFIG } from './config.js';
-import { input, inputTime } from './input.js';
+import { input } from './input.js';
+
 import { Player } from './player.js';
 import { drawPlayer } from './renderer.js';
+import { GameMap } from './map.js';
+import { Camera } from './camera.js';
 
 const player = new Player();
-const debugEl = document.getElementById('input-debug');
-
-/* MUSS IDENTISCH ZU CSS SEIN */
-const UI_HEIGHT = 240;
-const MAP_LIMIT_Y = CONFIG.HEIGHT - UI_HEIGHT;
+const gameMap = new GameMap();
+const camera = new Camera();
 
 function update() {
   player.update(input);
-
-  /* Harte Map-Grenze */
-  if (player.y + player.height > MAP_LIMIT_Y) {
-    player.y = MAP_LIMIT_Y - player.height;
-  }
-
-  /* Input-Timer */
-  for (const key in input) {
-    if (input[key]) {
-      inputTime[key] += 1 / 60;
-    }
-  }
+  camera.follow(player);
 }
 
 function render() {
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, CONFIG.WIDTH, CONFIG.HEIGHT);
 
+  gameMap.draw(ctx, camera);
   drawPlayer(ctx, player);
-
-  /* Debug */
-  debugEl.innerHTML = '';
-  for (const key in inputTime) {
-    if (inputTime[key] > 0) {
-      const line = document.createElement('div');
-      line.textContent = `${key} : ${inputTime[key].toFixed(2)}s`;
-      debugEl.appendChild(line);
-    }
-  }
 }
 
 function gameLoop() {
