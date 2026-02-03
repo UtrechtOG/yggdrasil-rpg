adcab4729e00ad5edd3f4f0ec75f318a9b018e18
🌐 UtrechtOG 🌐 <Calimali2006@gmail.com>
2026-02-03 10:33:15 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,18 +1,17 @@
 // src/main.js
 import './keyboard.js';
+import './touch.js';
+
 import { ctx } from './canvas.js';
 import { CONFIG } from './config.js';
 import { input } from './input.js';
 
-function update() {
-  // später Player-Logik
-}
+function update() {}
 
 function render() {
   ctx.fillStyle = 'black';
   ctx.fillRect(0, 0, CONFIG.WIDTH, CONFIG.HEIGHT);
 
-  // sichtbares Debug (Android!)
   ctx.fillStyle = 'white';
   ctx.font = '16px monospace';
   ctx.fillText('INPUT:', 20, 30);
